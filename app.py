fc7f6023c1008da7c35c9491974a133123ca45ed
Korda53 <bertazsolt1990@gmail.com>
2024-10-31 23:07:23 +0100

    port number

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import xml.etree.ElementTree as ET
 import random
+import os
 
 app = Flask(__name__)
 
@@ -129,4 +130,5 @@ def delete_player():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))  # Get the port from environment or use 5000 for local
+    app.run(host="0.0.0.0", port=port)
